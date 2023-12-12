823f0d331118b264afad582d6f38a3d47847a9a2
Andrew Dame <andrewdame@google.com>
2023-12-11 23:32:56 -0500

    feat: Decouple express app creation from deployment

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,7 @@
-import express, { Request, Response } from "express";
-import bodyParser from "body-parser";
-import fileUpload from "express-fileupload";
-import cookieParser from "cookie-parser";
-import path from "path";
-import constructAuthRoutes from "./routes/auth.routes";
-import constructUserRoutes from "./routes/user.routes";
-import constructProjectRoutes from "./routes/project.routes";
-import constructSegmentRoutes from "./routes/segment.routes";
-import constructIssueRoutes from "./routes/issue.routes";
-import { constructBottle } from "./bottle";
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, "../../build")));
-app.use(bodyParser());
-app.use(cookieParser());
-app.use(express.urlencoded({
-  extended: true,
-}));
-app.use(fileUpload({
-  createParentPath: true,
-}));
-
-const bottle = constructBottle();
-
-constructAuthRoutes(app, bottle);
-constructUserRoutes(app, bottle);
-constructProjectRoutes(app, bottle);
-constructSegmentRoutes(app, bottle);
-constructIssueRoutes(app, bottle);
-
-app.get("*", (_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../build", "index.html"));
-});
+import { constructApp } from "./app";
 
 let port: any;
+const app = constructApp();
 
 if (process.env.APP_ENV === "development") {
   port = 8081;
